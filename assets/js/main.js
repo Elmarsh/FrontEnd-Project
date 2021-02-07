1972976277d7ceff0356def1e3d404bdb87d9895
Elmar Abdullayev <elmarsha@code.edu.az>
2021-02-07 14:21:45 +0400

    created footer.scss, blog.scss,news.scss,;  update index.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,3 +15,19 @@ $('.owl-carousel').owlCarousel({
     }
 });
 
+$('.blog-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+});
