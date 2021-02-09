46b7d61fbf1ed42b664182bec3342ac77dd9f0f1
Elmar Abdullayev <elmarsha@code.edu.az>
2021-02-09 20:24:53 +0400

    main.js update, website update,   index.html update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,62 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1500:{
-            items:5
+$(document).ready(function(){
+    $('.category-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:3
+            }
         }
-    }
+        
+    });
+    $(window).scroll(function (e) {
+        if ($(this).scrollTop() > 84) {
+            $("header nav").addClass('header-fixed');
+            $("#back2Top").fadeIn();
+        } else {
+            $("header nav").removeClass('header-fixed');
+            $("#back2Top").fadeOut();
+        }
+    });
     
-});
-
-$('.blog-carousel').owlCarousel({
-    loop: true,
-    margin: 30,
-    nav: false,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 2
-        },
-        1000: {
-            items: 3
+    $('.testimonials-carousel').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 4
+            }
         }
-    }
+    });
+    
+    $('.blog-carousel').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
 });
